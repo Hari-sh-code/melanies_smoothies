3d2c466c7e638899c614a898d893a2a9bb5b822f
Hari-sh-code <harishrn2004@gmail.com>
2025-02-01 23:43:18 +0530

    Response lib added

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,6 +73,11 @@ if time_to_insert:
         except Exception as e:
             st.error(f"Error submitting order: {e}")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if st.session_state.selected_ingrediants:
+    ingrediants_string_response = ''
+
+    for fruit_chosen in st.session_state.selected_ingrediants:
+        ingrediants_string_response +=fruit_chosen + ''
+        st.subheader(fruit_chosen+' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
