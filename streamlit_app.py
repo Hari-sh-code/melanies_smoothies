12c6153cbfd95d50049248e103e6e4f19e4a8adc
Hari-sh-code <harishrn2004@gmail.com>
2025-02-01 23:11:44 +0530

    Response lib added

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,4 +75,4 @@ if time_to_insert:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
